51a133ed42fb84dc09a92131ec65aa1c682f7bc1
SGospodinov <sani9706@gmail.com>
2016-05-03 23:05:24 +0300

    adding beatable AI and single play mode

--- tic_tac_toe/tic_tac_toe.ino
+++ tic_tac_toe/tic_tac_toe.ino
@@ -1,16 +1,25 @@
+#define SINGLE_MODE 1
+#define TWO_PLAYERS_MODE 2
+#define EMPTY 0
+#define X_ 1
+#define O_ 2
+#define NO_WINNER 0
+#define BOARD_SIZE 9
+
 int indicatorLedBlue = 22;
 int indicatorLedRed = 23;
 boolean turn = true;
+int gameMode = SINGLE_MODE;
 int row = 0;
-int board[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-int oldBoard[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int board[] = {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
+int oldBoard[] = {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
 int redLeds [] = {11, 12, 13, 14, 15, 16, 17, 18, 19};
 int blueLeds [] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 int buttonPins[] = {53, 52, 51, 50, 49, 48, 47, 46, 45};
 
 void setup(){
   Serial.begin(9600);
-  for(int i = 0; i < 9; i++){
+  for(int i = 0; i < BOARD_SIZE; i++){
     pinMode(buttonPins[i], INPUT);
     digitalWrite(buttonPins[i], HIGH);
     pinMode(redLeds[i], OUTPUT);
@@ -96,7 +105,7 @@ int checkButton(int button) {
 
 int readButtons(){
   while(1){
-    for (int i = 0; i < 9; i++){
+    for (int i = 0; i < BOARD_SIZE; i++){
       int a = checkButton(i);
       if (a > 9)
         return a;
@@ -118,18 +127,18 @@ void changePlayer(){
   
 
 void play(int value){
-  if(board[value]!=0){
+  if(board[value] != EMPTY){
     return;
   }
-  for(int i = 0; i < 9; i++){
+  for(int i = 0; i < BOARD_SIZE; i++){
     oldBoard[i] = board[i];
   }
   if(turn){
     digitalWrite(redLeds[value],HIGH);
-    board[value] = 1;
+    board[value] = X_;
   } else{
     digitalWrite(blueLeds[value],HIGH);
-    board[value] = 2;
+    board[value] = O_;
   }
   changePlayer();
 }
@@ -162,29 +171,29 @@ void winning(int winner){
     digitalWrite(indicatorLedRed, LOW);
   }
   if(winner != 1){
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < BOARD_SIZE; i++){
       digitalWrite(redLeds[i], LOW);
     }
     for(int i = 0; i < 5; i++){
-      for(int j = 0; j < 9; j++){
+      for(int j = 0; j < BOARD_SIZE; j++){
         digitalWrite(blueLeds[j], HIGH);
       }
       delay(500);
-      for(int j = 0; j < 9; j++){
+      for(int j = 0; j < BOARD_SIZE; j++){
         digitalWrite(blueLeds[j], LOW);
       }
       delay(500);
     }
   }else{
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < BOARD_SIZE; i++){
       digitalWrite(blueLeds[i], LOW);
     }
     for(int i = 0; i < 5; i++){
-      for(int j = 0; j < 9; j++){
+      for(int j = 0; j < BOARD_SIZE; j++){
         digitalWrite(redLeds[j], HIGH);
       }
       delay(500);
-      for(int j = 0; j < 9; j++){
+      for(int j = 0; j < BOARD_SIZE; j++){
         digitalWrite(redLeds[j], LOW);
       }
       delay(500);
@@ -199,21 +208,21 @@ void drow_blink(){
   } else{
     digitalWrite(indicatorLedRed, LOW);
   }
-  for(int i = 0; i<9; i++){
-  if(board[i] == 1){
+  for(int i = 0; i<BOARD_SIZE; i++){
+  if(board[i] == X_){
     digitalWrite(redLeds[i], LOW);
-  }else if(board[i] == 2)
+  }else if(board[i] == O_)
     digitalWrite(blueLeds[i], LOW);
   }
   for(int j = 0; j < 5; j++){
-    for(int i = 0; i<9; i++){
+    for(int i = 0; i<BOARD_SIZE; i++){
       if(board[i] == 1){
         digitalWrite(redLeds[i], HIGH);
       }else if(board[i] == 2)
         digitalWrite(blueLeds[i], HIGH);
     }
     delay(500);
-    for(int i = 0; i<9; i++){
+    for(int i = 0; i<BOARD_SIZE; i++){
       if(board[i] == 1){
         digitalWrite(redLeds[i], LOW);
       }else if(board[i] == 2)
@@ -225,7 +234,7 @@ void drow_blink(){
 }
 
 void undo(){
-  for(int i = 0; i < 9; i++){
+  for(int i = 0; i < BOARD_SIZE; i++){
     board[i] = oldBoard[i];
   }
   if(turn){
@@ -237,7 +246,7 @@ void undo(){
   }
   turn = !turn;
   row--;
-  for(int i = 0; i<9; i++){
+  for(int i = 0; i<BOARD_SIZE; i++){
     if(board[i] == 0){
       digitalWrite(redLeds[i], LOW);
       digitalWrite(blueLeds[i], LOW);
@@ -249,7 +258,7 @@ void undo(){
 void restartGame(){
   turn = true;
   row = 0;
-  for(int i = 0; i < 9; i++){
+  for(int i = 0; i < BOARD_SIZE; i++){
     digitalWrite(redLeds[i], LOW);
     digitalWrite(blueLeds[i], LOW);
     board[i] = 0;
@@ -263,21 +272,48 @@ void decodeButtonVlues(int value){
   int button = value%10;
   if(event == 1){
     play(button);
-  }else if(event == 2){
+  }else if(event == 2 && gameMode == TWO_PLAYERS_MODE){
     undo();
   }else if(event == 3){
     restartGame();
   }
 }
 
+int computeTurn(){
+  int positions[8];
+  int positions_count = 0; 
+  for(int i = 0; i < BOARD_SIZE; i++){
+    if(board[i] == EMPTY){
+      positions[positions_count++] = i;
+    }
+  }
+  for(int i = 0; i < positions_count; i++){
+    int current_position = positions[i];
+    board[current_position] = O_;
+    if(checkForWinner() == O_){
+      board[current_position] = EMPTY;
+      return current_position;
+    }
+    board[current_position] = EMPTY;
+  }
+  return positions[0];
+}
+
 void loop(){
-  decodeButtonVlues(readButtons());
+  if(gameMode == TWO_PLAYERS_MODE || (gameMode == SINGLE_MODE && turn)){
+    decodeButtonVlues(readButtons());
+  }else{
+    int arduinoTurn = computeTurn();
+    Serial.print("Aduino turn: ");
+    Serial.println(arduinoTurn);
+    play(arduinoTurn);
+  }
   int winner = checkForWinner();
-  if(row == 10 && winner == 0){
+  if(row == 10 && winner == NO_WINNER){
     drow_blink();
     restartGame();
   }
-  if(winner != 0){
+  if(winner != NO_WINNER){
     winning(winner);
     restartGame();
   }
